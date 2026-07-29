9a8e1c0655335912b160317c77683ff092b8d4a4
Benedict Omojefe <benworldaskills098@gmail.com>
2026-07-28 20:56:10 -0700

    Chore: Custom UserManager + AUTH_USER_MODEL wiring (#8)
    * Chore: Custom UserManager + AUTH_USER_MODEL wiring
    
    * fix: implement custom UserManager

--- accounts/models.py
+++ accounts/models.py
@@ -1,23 +1,50 @@
 import uuid
 
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
 
 class RoleOptions(models.TextChoices):
-        USER = 'USER', 'User'
-        ADMIN = 'ADMIN', 'Admin'
-        MODERATOR = 'MODERATOR', 'Moderator'
+    USER = 'USER', 'User'
+    ADMIN = 'ADMIN', 'Admin'
+    MODERATOR = 'MODERATOR', 'Moderator'
+
 
 class StatusOptions(models.TextChoices):
-        ACTIVE = 'ACTIVE', 'Active',
-        INACTIVE = 'INACTIVE', 'Inactive',
-        SUSPENDED = 'SUSPENDED', 'Suspended',
-        PENDING_VERIFICATION = 'PENDING_VERIFICATION', 'Pending_verification'
+    ACTIVE = 'ACTIVE', 'Active',
+    INACTIVE = 'INACTIVE', 'Inactive',
+    SUSPENDED = 'SUSPENDED', 'Suspended',
+    PENDING_VERIFICATION = 'PENDING_VERIFICATION', 'Pending_verification'
+
 
 class ProviderOptions(models.TextChoices):
-        LOCAL = 'LOCAL', 'Local',
-        GOOGLE = 'GOOGLE', 'Google',
-        GITHUB = 'GITHUB', 'Github'
+    LOCAL = 'LOCAL', 'Local',
+    GOOGLE = 'GOOGLE', 'Google',
+    GITHUB = 'GITHUB', 'Github'
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("role", RoleOptions.ADMIN)
+        extra_fields.setdefault("status", StatusOptions.ACTIVE)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser most have is_superuser=True")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -45,15 +72,17 @@ class User(AbstractBaseUser, PermissionsMixin):
         choices=ProviderOptions.choices,
         default=ProviderOptions.LOCAL
     )
-    provider_id = models.CharField(max_length=100, null=True, blank=True, db_index=True)
+    provider_id = models.CharField(
+        max_length=100, null=True, blank=True, db_index=True)
     last_login_at = models.DateTimeField(null=True, )
     last_login_ip = models.GenericIPAddressField(null=True, blank=True)
     login_attempts = models.IntegerField(default=0)
     locked_until = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    manager = UserManager()
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["name"]
 
-def __str__(self):
+    def __str__(self):
         return self.email
